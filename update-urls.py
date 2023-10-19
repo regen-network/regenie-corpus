4e3192bf9783f93712cd99a25880b9cbb3bf0bc9
Aaron Craelius <aaronc@users.noreply.github.com>
2023-10-19 12:32:51 -0400

    update script add gitbook pdf's

--- update-urls.py
+++ update-urls.py
@@ -18,11 +18,33 @@ def write_urls_to_csv(urls, filename):
             csv_writer.writerow([url])
 
 
-def update_csv(sitemap_url, filename):
-    urls = fetch_sitemap_urls(sitemap_url)
+def update_sitemap_csv(site):
+    urls = fetch_sitemap_urls('https://' + site + '/sitemap.xml')
+    filename = 'managed-urls/' + site + '.csv'
     write_urls_to_csv(urls, filename)
 
 
-update_csv('https://docs.regen.network/sitemap.xml', 'docs.csv')
-update_csv('https://registry.regen.network/sitemap.xml', 'registry.csv')
-update_csv('https://guides.regen.network/sitemap.xml', 'guides.csv')
+def get_github_download_urls(owner, repo, path):
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        contents = response.json()
+        urls = [item['download_url'] for item in contents if item['download_url'].endswith('.pdf')]
+        return urls
+    else:
+        print(f"Failed with status code: {response.status_code}. Message: {response.text}")
+
+
+def update_github_download_csv(filename, owner, repo, path):
+    urls = get_github_download_urls(owner, repo, path)
+    filename = 'managed-urls/' + filename + '.csv'
+    write_urls_to_csv(urls, filename)
+
+
+update_sitemap_csv('docs.regen.network')
+update_sitemap_csv('registry.regen.network')
+update_sitemap_csv('guides.regen.network')
+
+update_github_download_csv('registry-credit-class-pdfs', 'regen-network', 'regen-registry-credit-classes', '.gitbook/assets')
+update_github_download_csv('registry-handbook-pdfs', 'regen-network', 'regen-registry-handbook', '.gitbook/assets')
